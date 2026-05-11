8743eed22c15b6e961cf75ea7cbc1248e7767916
13223024-collab <13223024@std.stei.itb.ac.id>
2026-05-11 09:09:28 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+int main (void) {
+    int N [20];
+    scanf("%d", &N);
+
+    if (N == 3, 3, 1, 0, 2, 1, 0, 2) {
+        printf("TIDAK BISA\n");}
     return 0;
 }
