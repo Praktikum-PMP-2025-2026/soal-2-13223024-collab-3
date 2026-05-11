56d73b67fb0356a389539c53f9fb57c2385d8795
13223024-collab <13223024@std.stei.itb.ac.id>
2026-05-11 09:52:52 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,3 +1,14 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 5 - Foundation of Algorithm
+ *   Hari dan Tanggal    : Senin, 11 Mei 2026
+ *   Nama (NIM)          : Muhammad 'Afif Yusuf (13223024)
+ *   Nama File           : soal2.c
+ *   Deskripsi           : Menentukan bisa tidaknya berdasarkan berdasarka susunan prasyarat misi yang diberikan dengan
+ *                         menggunakan pendekatan algoritma atau graph
+ * 
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
